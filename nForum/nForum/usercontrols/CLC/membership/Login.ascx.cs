8c632712e34f53815ed803afef7a01b6c7f65cd9
jeffjah88 <jeffjah88@e80ff43e-a6a0-68f0-4ba4-4d58d44e2c90>
2012-06-03 15:55:06 +0000

    Loginshizzle toegevoegd, woei! Uurtjes bijgewerkt.

---
+++ nForum/nForum/usercontrols/CLC/membership/Login.ascx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using nForum.BusinessLogic;
+using umbraco;
+using umbraco.cms.businesslogic.member;
+
+namespace nForum.usercontrols.CLC.membership
+{
+    public partial class Login : BaseForumUsercontrol
+    {
+
+        protected void OnLoginError(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Concat(CurrentPageAbsoluteUrl, "?m=", ctlLogin.FailureText));
+        }
+
+        protected void OnLoggedIn(object sender, EventArgs e)
+        {
+            // Get the user
+            var m = Member.GetMemberFromLoginName(ctlLogin.UserName);
+            // Check if user is banned or not
+            if (m.getProperty("forumUserIsBanned").Value.ToString() == "1")
+            {
+                MembershipHelper.LogoutMember();
+                Response.Redirect(string.Concat(CurrentPageAbsoluteUrl, "?m=", library.GetDictionaryItem("AccountBanned")));
+            }
+
+            // Check if user is authorised
+            if(Settings.ManuallyAuthoriseNewMembers)
+            {
+                // if user is not authorised, logout and return a message
+                if (m.getProperty("forumUserIsAuthorised").Value.ToString() != "1")
+                {
+                    MembershipHelper.LogoutMember();
+                    Response.Redirect(string.Concat(CurrentPageAbsoluteUrl, "?m=", library.GetDictionaryItem("AccountNotAuth")));
+                }
+            }
+       
+            // Successful login, so redirect to forum root
+            Response.Redirect(Settings.Url);
+
+        }
+
+
+    }
+}
---
+++ nForum/nForum/usercontrols/CLC/membership/UserAdmin.ascx
@@ -0,0 +1,9 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UserAdmin.ascx.cs" Inherits="nForum.usercontrols.CLC.membership.UserAdmin" %>
+	 <asp:LoginStatus ID="ctlLogin"
+                     runat="server"
+                     LoginText="inloggen"
+                     LogoutText="uitloggen" OnLoggedOut="UmbracoLogout" />
+					 <span>|</span>
+					 <asp:LoginName ID="ctlLoginName" runat="server" />
+					 <asp:HyperLink ID="lnkInschrijven" runat="server" Text="inschrijven" />
+	<%--<a href="/inloggen.aspx">inloggen</a> <span>|</span> <a href="#">inschrijven</a>--%>
