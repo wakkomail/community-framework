84363f57248ae9f43b8b374117a68da008ecdee9
jeffjah88 <jeffjah88@e80ff43e-a6a0-68f0-4ba4-4d58d44e2c90>
2012-06-20 21:07:56 +0000

    Begint gemaakt met projectenrepeater op de homepage. Geen zin meer nu, dus het werkt nog niet. Bite me.

---
+++ trunk/nForum/nForum/usercontrols/CLC/Projects.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using nForum.BusinessLogic;
+using nForum.BusinessLogic.Models;
+using umbraco;
+using nForum.helpers;
+
+namespace nForum.usercontrols.CLC
+{
+    public partial class Projects : BaseForumUsercontrol
+	{
+		
+
+		protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+				InitializeProjects();
+            }
+        }
+
+		private void InitializeProjects()
+        {
+			//Get the projects rootfolder.
+			var projectsRoot = DocumentHelper.GetOrCreateProjectgroupCategory();
+			
+        }
+    }
+}
