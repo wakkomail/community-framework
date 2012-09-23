c6a019c4671c2d1758fde984e54cf01cd4ffeb08
jeffjah88 <jeffjah88@gmail.com>
2012-09-23 10:26:47 +0000

    Inschrijflink is foetsie, inlogtekst is beheerbaar!

--- nForum/nForum/usercontrols/CLC/membership/Login.ascx.designer.cs
+++ nForum/nForum/usercontrols/CLC/membership/Login.ascx.designer.cs
@@ -20,5 +20,14 @@ namespace nForum.usercontrols.CLC.membership {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Login ctlLogin;
+        
+        /// <summary>
+        /// litLoginDescription control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litLoginDescription;
     }
 }
--- nForum/nForum/usercontrols/CLC/membership/UserAdmin.ascx.designer.cs
+++ nForum/nForum/usercontrols/CLC/membership/UserAdmin.ascx.designer.cs
@@ -22,6 +22,15 @@ namespace nForum.usercontrols.CLC.membership {
         protected global::System.Web.UI.WebControls.LoginStatus ctlLogin;
         
         /// <summary>
+        /// litSeparator control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litSeparator;
+        
+        /// <summary>
         /// ctlLoginName control.
         /// </summary>
         /// <remarks>
