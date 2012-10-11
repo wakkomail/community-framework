e00ec9a5c58b08fb33294d6e6b0497d8e57eaf55
jeffjah88 <jeffjah88@e80ff43e-a6a0-68f0-4ba4-4d58d44e2c90>
2012-10-11 19:08:36 +0000

    Populaire projecten toegevoegd, media embed meuk. Joepie!

---
+++ nForum/nForum/usercontrols/CLC/PopularProjects.ascx
@@ -0,0 +1,17 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="PopularProjects.ascx.cs" Inherits="nForum.usercontrols.CLC.PopularProjects" %>
+<%@ Import Namespace="nForum.BusinessLogic.Models" %>
+
+<div class="fatBorder" id="relevanteProjectenContainer">
+    <h3 class="kaffeeSatz c2">Populaire projecten</h3>
+    <ul>
+            <asp:Repeater ID="rptMainForumList" runat="server" EnableViewState="false">
+                <ItemTemplate>
+                    <li>
+                        <a href="<%# ((ForumCategory)Container.DataItem).Url%>" class="c2">
+                            <%# ((ForumCategory)Container.DataItem).Name%>
+                        </a>
+                    </li>
+                </ItemTemplate>
+            </asp:Repeater>
+    </ul>
+</div>
---
+++ nForum/nForum/usercontrols/CLC/PopularProjects.ascx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using nForum.BusinessLogic;
+using nForum.BusinessLogic.Models;
+using umbraco;
+using nForum.helpers;
+using umbraco.NodeFactory;
+using System.Collections.Generic;
+
+namespace nForum.usercontrols.CLC
+{
+    public partial class PopularProjects : BaseForumUsercontrol
+	{
+        public int AmountToTake { get; set; }
+
+		protected bool UseGridClass
+		{
+			get
+			{
+				return AmountToTake <= 0;
+			}
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                CreateMainForumList();
+            }
+        }
+
+        private void CreateMainForumList()
+        {
+			//Get all projects.
+			var projects = DocumentHelper.GetOrCreateProjectgroupCategory().Children;
+			var projectCategories = new List<ForumCategory>();
+
+			foreach (var project in projects)
+			{
+				 projectCategories.Add(Mapper.MapForumCategory(new Node(project.Id)));
+			}
+
+			var popularMembergroups = projectCategories.Where(m => m.SubTopics().Count(t => t.Posts().Count(p => p.CreatedOn > DateTime.Now.AddDays(-14)) > 5) >= 1);
+
+            if (AmountToTake > 0)
+            {
+				popularMembergroups = popularMembergroups.Take(AmountToTake);
+            }
+
+			if (popularMembergroups.Any())
+            {
+				rptMainForumList.DataSource = popularMembergroups;
+                rptMainForumList.DataBind();
+            }
+        }
+    }
+}
